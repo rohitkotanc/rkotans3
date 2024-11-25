325b9a3a1058be425339d18c2f36e372d96894cb
rohitkotanc <rkotanc@gmail.com>
2024-11-25 13:22:35 -0500

    added cc file

---
+++ ns-3.38/myexamples_cc/two-car-building-proper-connection-dip.cc
@@ -0,0 +1,142 @@
+#include <ns3/applications-module.h>
+#include <ns3/core-module.h>
+#include <ns3/internet-module.h>
+#include <ns3/mobility-module.h>
+#include <ns3/netsimulyzer-module.h>
+#include <ns3/network-module.h>
+#include <ns3/point-to-point-module.h>
+#include <ns3/netanim-module.h>
+#include <ns3/buildings-module.h>
+#include <iostream>
+
+using namespace ns3;
+
+
+void WriteThroughput(Ptr<netsimulyzer::ThroughputSink> sink, Ptr<const Packet> packet)
+{
+    sink->AddPacket(packet);
+}
+
+
+void DropConnectivity(Ptr<NetDevice> clientDevice, Ptr<NetDevice> serverDevice)
+{
+    Ptr<PointToPointNetDevice> clientP2PDevice = DynamicCast<PointToPointNetDevice>(clientDevice);
+    Ptr<PointToPointNetDevice> serverP2PDevice = DynamicCast<PointToPointNetDevice>(serverDevice);
+    clientP2PDevice->GetChannel()->SetAttribute("Delay", TimeValue(MilliSeconds(100)));
+    serverP2PDevice->GetChannel()->SetAttribute("Delay", TimeValue(MilliSeconds(100)));
+    std::cout << "Connectivity drop (delay introduced) at: " << Simulator::Now().GetSeconds() << " seconds." << std::endl;
+}
+
+void RestoreConnectivity(Ptr<NetDevice> clientDevice, Ptr<NetDevice> serverDevice)
+{
+    Ptr<PointToPointNetDevice> clientP2PDevice = DynamicCast<PointToPointNetDevice>(clientDevice);
+    Ptr<PointToPointNetDevice> serverP2PDevice = DynamicCast<PointToPointNetDevice>(serverDevice);
+    clientP2PDevice->GetChannel()->SetAttribute("Delay", TimeValue(MilliSeconds(100)));
+    serverP2PDevice->GetChannel()->SetAttribute("Delay", TimeValue(MilliSeconds(100)));
+    std::cout << "Connectivity restored (delay removed) at: " << Simulator::Now().GetSeconds() << " seconds." << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    double durationUser = 20.0;
+    std::string outputFileName = "two-car-building-proper-connection-dip.json";
+
+    CommandLine cmd(__FILE__);
+    cmd.AddValue("outputFileName", "The name of the file to write the NetSimulyzer trace info", outputFileName);
+    cmd.AddValue("duration", "Duration (in seconds) of the simulation", durationUser);
+    cmd.Parse(argc, argv);
+
+    NS_ABORT_MSG_IF(durationUser < 3.0, "Scenario must be at least three seconds long");
+    const auto duration = Seconds(durationUser);
+
+    NS_ABORT_MSG_IF(outputFileName.empty(), "`outputFileName` must not be empty");
+
+    NodeContainer nodes;
+    nodes.Create(2);  
+
+    MobilityHelper mobility;
+    Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator>();
+    positionAlloc->Add(Vector3D(25.0, 5.0, 0.0));
+    positionAlloc->Add(Vector3D(5.0, 5.0, 0.0));
+    mobility.SetPositionAllocator(positionAlloc);
+    mobility.SetMobilityModel("ns3::ConstantVelocityMobilityModel");
+    mobility.Install(nodes);
+
+    nodes.Get(0)->GetObject<ConstantVelocityMobilityModel>()->SetVelocity(Vector(0.0, -2.5, 0.0));
+    nodes.Get(1)->GetObject<ConstantVelocityMobilityModel>()->SetVelocity(Vector(0.0, -2.5, 0.0));
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("0ms"));
+
+    NetDeviceContainer netDevices = pointToPoint.Install(nodes.Get(0), nodes.Get(1));
+
+    InternetStackHelper stack;
+    stack.Install(nodes.Get(0));
+    stack.Install(nodes.Get(1));
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer interfaces = address.Assign(netDevices);
+
+    const uint16_t echoPort = 9;
+    UdpEchoServerHelper echoServer(echoPort);
+    ApplicationContainer serverApp = echoServer.Install(nodes.Get(1));
+    serverApp.Start(Seconds(1.0));
+    serverApp.Stop(duration - Seconds(1.0));
+
+    UdpEchoClientHelper echoClient(interfaces.GetAddress(1), echoPort);
+    double interval = 0.1;
+    echoClient.SetAttribute("MaxPackets", UintegerValue(10000));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(interval)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApp = echoClient.Install(nodes.Get(0));
+    clientApp.Start(Seconds(1.0));
+    clientApp.Stop(duration - Seconds(1.0));
+
+    Simulator::Schedule(Seconds(6.0), &DropConnectivity, netDevices.Get(0), netDevices.Get(1));
+    Simulator::Schedule(Seconds(7.0), &RestoreConnectivity, netDevices.Get(0), netDevices.Get(1));
+
+    Ptr<netsimulyzer::Orchestrator> orchestrator = CreateObject<netsimulyzer::Orchestrator>(outputFileName);
+    orchestrator->SetTimeStep(MilliSeconds(100), Time::MS);
+    orchestrator->SetAttribute("PollMobility", BooleanValue(true));
+
+    netsimulyzer::NodeConfigurationHelper nodeHelper(orchestrator);
+    nodeHelper.Set("Model", netsimulyzer::models::CAR_VALUE);
+    nodeHelper.Set("HighlightColor", netsimulyzer::OptionalValue<netsimulyzer::Color3>(netsimulyzer::BLUE));
+    nodeHelper.Install(nodes.Get(0));
+
+    nodeHelper.Set("HighlightColor", netsimulyzer::OptionalValue<netsimulyzer::Color3>(netsimulyzer::RED));
+    nodeHelper.Install(nodes.Get(1));
+
+    Ptr<Building> building = CreateObject<Building>();
+    building->SetBoundaries(Box(15.0, 20.0, -20.0, -15.0, 0.0, 15.0));
+    building->SetBuildingType(Building::Residential);
+    building->SetExtWallsType(Building::ConcreteWithWindows);
+    building->SetNFloors(3);
+    building->SetNRoomsX(2);
+    building->SetNRoomsY(2);
+
+    BuildingContainer buildingContainer;
+    buildingContainer.Add(building);
+
+    netsimulyzer::BuildingConfigurationHelper buildingHelper(orchestrator);
+    buildingHelper.Install(buildingContainer);
+
+    Ptr<netsimulyzer::ThroughputSink> clientThroughput = CreateObject<netsimulyzer::ThroughputSink>(orchestrator, "UDP Echo Client Throughput (RX)");
+    clientThroughput->GetSeries()->SetAttribute("Color", netsimulyzer::BLUE_VALUE);
+    clientApp.Get(0)->TraceConnectWithoutContext("Rx", MakeBoundCallback(&WriteThroughput, clientThroughput));
+
+    Ptr<netsimulyzer::ThroughputSink> serverThroughput = CreateObject<netsimulyzer::ThroughputSink>(orchestrator, "UDP Echo Server Throughput (RX)");
+    serverThroughput->GetSeries()->SetAttribute("Color", netsimulyzer::RED_VALUE);
+    serverApp.Get(0)->TraceConnectWithoutContext("Rx", MakeBoundCallback(&WriteThroughput, serverThroughput));
+
+    Simulator::Stop(duration);
+    Simulator::Run();
+    Simulator::Destroy();
+
+    return 0;
+}
+
+
